07ba59d9a7acd4b95ad16866190f219d01693285
john cotamo <johncotamo@hotmail.com>
2023-09-19 15:13:33 -0500

    se añade fetch

---
+++ src/bases/10-fetch.js
@@ -0,0 +1,28 @@
+const apiKey = "a4zJGDfGOZ1Nsix5yreFUbm4QLUBg7QW";
+
+const peticion = fetch(
+  `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
+);
+
+// peticion
+//   .then((resp) => {
+//     resp.json().then((data) => {
+//       console.log(data);
+//     });
+//   })
+
+//   .catch(console.warn);
+
+//promesa en cadena
+peticion
+  .then((resp) => resp.json())
+  .then(({ data }) => {
+    // console.log(data.images.original.url)
+    const { url } = data.images.original;
+
+    const img = document.createElement("img");
+    img.src = url;
+
+    document.body.append(img);
+  })
+  .catch(console.warn);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-const apiKey = "a4zJGDfGOZ1Nsix5yreFUbm4QLUBg7QW";
-
-const peticion = fetch(
-  `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
-);
-
-peticion
-  .then((resp) => {
-    resp.json().then((data) => {
-      console.log(data);
-    });
-  })
-
-  .catch(console.warn);
+console.log()
